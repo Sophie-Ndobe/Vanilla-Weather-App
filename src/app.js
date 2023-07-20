97f28034f3acde003e89b01eeb7f422fab4380b3
Sophie-Ndobe <ndobes76@gmail.com>
2023-07-20 14:12:12 +0200

    Added updating time and day in JavaScript

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
+let currentDate = new Date();
 
+let hours = currentDate.getHours();
+
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let minutes = currentDate.getMinutes();
+
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[currentDate.getDay()];
+
+let updatingDate = document.querySelector("#date");
+updatingDate.innerHTML = `${day} ${hours}:${minutes}`;
